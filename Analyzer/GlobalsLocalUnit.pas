d011499c01994c2409f51f28313bbcfcdad46de7
Pavel Perminov <packpaul@mail.ru>
2011-10-08 12:14:51 +0000

    Chess4Net_Analyzer: version update -> 2011.3 alpha
    git-svn-id: file:///media/sf_HostShared/projects/Repositories/CHESS4NET/Current@327 9f6b27b3-5d70-b942-b751-2d5c312fce45

--- Analyzer/GlobalsLocalUnit.pas
+++ Analyzer/GlobalsLocalUnit.pas
@@ -9,8 +9,8 @@ unit GlobalsLocalUnit;
 interface
 
 const
-  CHESS4NET_VERSION = 201102; // 2011.2
-  CHESS4NET_VERSION_TXT = '2011.2';
+  CHESS4NET_VERSION = 201103; // 2011.3
+  CHESS4NET_VERSION_TXT = '2011.3 alpha';
   COPYRIGHT_TXT = #169' 2007-2011 no rights reserved';
 
   CHESS4NET_URL = 'http://chess4net.ru';
